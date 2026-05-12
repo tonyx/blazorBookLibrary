875c98e48a3a61add93108de4db29e8adee6ba62
Tonino Lucca <tonyx1@gmail.com>
2026-05-12 17:25:15 +0200

    missing column

--- blazorBookLibrary.Server/vectorDbSetup/addTenant.sql
+++ blazorBookLibrary.Server/vectorDbSetup/addTenant.sql
@@ -1,5 +1,10 @@
 alter TABLE 
-    item_embeddings_projetions
+    item_embeddings_projections
     add column if not exists tenant_id uuid default '5a982f45-1c3a-4f7d-9a54-794ed7696f23'::uuid;
 
+
 create index if not exists idx_item_embedding_projections_tenant_id on item_embeddings_projections(tenant_id);
+
+alter TABLE
+    item_embeddings_projections
+    add column if not exists created_at timestamp without time zone;
